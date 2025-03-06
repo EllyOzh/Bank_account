34616ada4f2844258a0637328209e85bfeb3da4a
El'mira Ozherel'eva <elyasobirova@gmail.com>
2025-03-06 05:16:19 +0300

    restore flake8 processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,10 @@
 from typing import List, Dict
 from typing import Union
 
-def filter_by_state(info_client: List[Dict[str, Union[str, int]]], state: str = 'EXECUTED') -> List[Dict[str, Union[str, int]]]:
+
+def filter_by_state(
+        info_client: List[Dict[str, Union[str, int]]],
+        state: str = 'EXECUTED') -> List[Dict[str, Union[str, int]]]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
 state соответствует указанному значению."""
 
@@ -13,12 +16,14 @@ state соответствует указанному значению."""
     return new_list_dict
 
 
-def sort_by_date(info_client: List[Dict[str, Union[str, int]]], ascending: bool = 'True') -> List[Dict[str, Union[str, int]]]:
+def sort_by_date(
+                info_client: List[Dict[str, Union[str, int]]],
+                ascending: bool = 'True') -> List[Dict[str, Union[str, int]]]:
     """Функция возвращает новый список, отсортированный по дате."""
     sorted_date_ascending = []
     sorted_date_descending = []
     for i in info_client:
-        if ascending == False:
+        if ascending is False:
             sorted_date_descending = sorted(info_client, key=lambda i: i['date'], reverse=True)
             return sorted_date_descending
         else:
@@ -27,12 +32,14 @@ def sort_by_date(info_client: List[Dict[str, Union[str, int]]], ascending: bool 
 
 
 if __name__ == "__main__":
-    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                       {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                       {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+    print(filter_by_state(
+                         [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
-    print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    print(sort_by_date(
+                      [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
                        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
