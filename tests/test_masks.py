827574c1a520407e51b73ffcc00e2e1a69dc390c
El'mira Ozherel'eva <elyasobirova@gmail.com>
2025-03-15 10:34:03 +0300

    test final_ok masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,20 +1,20 @@
 from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
-@pytest.fixture
-def card_number(request):
-    return request.param
 
-@pytest.mark.parametrize('card_number', [7896541236985478965, 9685741236985, 7458963214523698], indirect = True)
+@pytest.mark.parametrize('num, expected', [
+    ('1234567890123456', '1234 56** **** 3456'),
+    (1234567890123456, '1234 56** **** 3456'),
+    (1234, '')
+])
+def test_get_mask_card_number_ok(num, expected):
+    assert get_mask_card_number(num) == expected
 
-def test_get_mask_card_number(card_number):
-    assert card_number in [7896541236985478965, 9685741236985, 7458963214523698]
+@pytest.mark.parametrize("account_num, expected", [
+    ('98765432109876543210', '**3210'),
+    (98765432109876543210, '**3210'),
+    (4321, '')
+])
+def test_get_mask_account_ok(account_num, expected):
+    assert get_mask_account(account_num) == expected
 
-@pytest.fixture
-def account_number(request):
-    return request.param
-
-@pytest.mark.parametrize('account_number', [25417811479632587412, 2521156511335, 'lalalal'], indirect = True)
-
-def test_get_mask_card_number(card_number):
-    assert card_number in [25417811479632587412, 2521156511335, 'lalalal']
