a790aff5da9da38613ff2cb1d6cbcdb9a83bd96b
El'mira Ozherel'eva <elyasobirova@gmail.com>
2025-03-16 01:30:33 +0300

    widget.py change

--- src/widget.py
+++ src/widget.py
@@ -17,11 +17,15 @@ def mask_account_card(number_card_or_account: str) -> str:
 
 def get_date(info_date: str) -> str:
     """Функция,  возвращающая строку с датой в формате 'ДД.ММ.ГГГГ'"""
-    date_form = datetime.strptime(info_date, "%Y-%m-%dT%H:%M:%S.%f")
-    return date_form.strftime("%d.%m.%Y")
+    try:
+        date_form = datetime.strptime(info_date, "%Y-%m-%dT%H:%M:%S.%f")
+        return date_form.strftime("%d.%m.%Y")
+    except ValueError:
+        return 'Введите дату в правильном формате'
 
 
 if __name__ == "__main__":
     print(mask_account_card(str("Счет 73654108430135874305")))
     print(get_date("2024-03-11T02:26:18.671407"))
+    print(get_date("2024-03"))
 
