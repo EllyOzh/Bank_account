953f8f5d3f1a6870eb3773cd41710a41d3ccbb18
El'mira Ozherel'eva <elyasobirova@gmail.com>
2025-03-16 01:02:22 +0300

    testing widget.py

---
+++ tests/test_widget.py
@@ -0,0 +1,33 @@
+from src.widget import mask_account_card, get_date
+import pytest
+
+@pytest.mark.parametrize('card_info, expected', [
+    ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
+    ('Счет 64686473678894779589', 'Счет **9589'),
+    ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
+    ('Счет 35383033474447895560', 'Счет **5560'),
+    ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
+    ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
+    ('Visa Gold 5999414228426353', 'Visa Gold 5999 41** **** 6353'),
+    ('Счет 73654108430135874305', 'Счет **4305')
+])
+def test_mask_account_card_ok(card_info, expected):
+    assert mask_account_card(card_info) == expected
+
+def test_mask_account_card_error_num():
+    with pytest.raises(Exception):
+        mask_account_card([])
+
+@pytest.mark.parametrize('data_info, expected', [
+    ('2024-03-11T02:26:18.671407', '11.03.2024')
+    ])
+def test_get_date_ok(data_info, expected):
+    assert get_date(data_info) == expected
+
+def test_get_date_error():
+    with pytest.raises(Exception):
+        get_date('2024-02-30:t68:70:25:75.3233')
+
+def test_get_date_error():
+    with pytest.raises(Exception):
+        get_date('  ')
