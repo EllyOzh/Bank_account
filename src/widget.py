819d3a17285273a4c584f5d6278c76e5a0519631
El'mira Ozherel'eva <elyasobirova@gmail.com>
2025-03-16 01:04:14 +0300

    mask_account change

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,6 @@ from black import datetime
 
 from src.masks import get_mask_account, get_mask_card_number
 
-
 def mask_account_card(number_card_or_account: str) -> str:
     """Функция, возвращающая замаскированный номер карты или счёта"""
     info_card_or_account = number_card_or_account.split()
@@ -18,7 +17,6 @@ def mask_account_card(number_card_or_account: str) -> str:
 
 def get_date(info_date: str) -> str:
     """Функция,  возвращающая строку с датой в формате 'ДД.ММ.ГГГГ'"""
-
     date_form = datetime.strptime(info_date, "%Y-%m-%dT%H:%M:%S.%f")
     return date_form.strftime("%d.%m.%Y")
 
@@ -26,3 +24,4 @@ def get_date(info_date: str) -> str:
 if __name__ == "__main__":
     print(mask_account_card(str("Счет 73654108430135874305")))
     print(get_date("2024-03-11T02:26:18.671407"))
+
